3aed5a1cd3b966aab2e3c2749ad523bbb1bc7b0f
RaulSierradev <illosierra@outlook.com>
2023-11-09 21:52:44 -0500

    Finalizado Header

--- prueba-front/src/components/Header/Header.jsx
+++ prueba-front/src/components/Header/Header.jsx
@@ -1,13 +1,33 @@
-import style from './Header.module.css'
+import style from "./Header.module.css";
 
 const Header = () => {
   return (
     <div className={style.header}>
       <ul className={style.nav}>
-        <li className={style.item}>Home</li>
-        <li className={style.item}>Pages</li>
-        <li className={style.item}>About</li>
-        <li className={style.item}>Contact</li>
+        <li className={style.item}>
+          <p>Home</p>
+        </li>
+        <li className={style.item}>
+          <p>Pages</p>
+          <ul className={style.dropdown}>
+            <li className={style.subItem}>Page 1</li>
+            <li className={style.subItem}>Page 2</li>
+            <li className={style.subItem}>
+              Page 3
+              <ul className={style.dropdownTwo}>
+                <li className={style.subItem2}>Item 1</li>
+                <li className={style.subItem2}>Item 1</li>
+                <li className={style.subItem2}>Item 1</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li className={style.item}>
+          <p>About</p>
+        </li>
+        <li className={style.item}>
+          <p>Contact</p>
+        </li>
       </ul>
     </div>
   );
